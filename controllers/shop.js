69a24221c7468fd74cdb3e2127888395ad71af0e
afsarahmad786 <mohommadafsar415@gmail.com>
2023-05-16 14:52:34 +0530

    orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -68,10 +68,10 @@ exports.postCart = (req, res, next) => {
   Product.findById(prodId)
     .then((product) => {
       req.user.addToCart(product);
-      res.redirect("/cart");
     })
     .then((result) => {
       console.log(result);
+      res.redirect("/cart");
     });
 };
 
@@ -87,29 +87,8 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
   req.user
-    .getCart()
-    .then((cart) => {
-      fetchedCart = cart;
-      return cart.getProducts();
-    })
-    .then((products) => {
-      return req.user
-        .createOrder()
-        .then((order) => {
-          return order.addProducts(
-            products.map((product) => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-              return product;
-            })
-          );
-        })
-        .catch((err) => console.log(err));
-    })
-    .then((result) => {
-      return fetchedCart.setProducts(null);
-    })
+    .addOrder()
     .then((result) => {
       res.redirect("/orders");
     })
@@ -118,7 +97,7 @@ exports.postOrder = (req, res, next) => {
 
 exports.getOrders = (req, res, next) => {
   req.user
-    .getOrders({ include: ["products"] })
+    .getOrders()
     .then((orders) => {
       res.render("shop/orders", {
         path: "/orders",
